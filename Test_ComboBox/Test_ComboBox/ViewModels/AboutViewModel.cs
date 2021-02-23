ed61df321bc699a2872d7c5e5fecf34d33c26102
MarcusCoding <voigt@berghof-systeme.de>
2021-02-23 22:07:15 +0100

    Projektdateien hinzufügen.

---
+++ Test_ComboBox/Test_ComboBox/ViewModels/AboutViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Test_ComboBox.Models;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Test_ComboBox.ViewModels
+{
+    public class AboutViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        string duration = "Item 1";
+
+        public string Duration
+        {
+            get => duration; set
+            {
+                if (value == null)
+                    return;
+                duration = value;
+            }
+        }
+        public IList<string> Durations => new List<string> { "Item 1", "Item 2", "Item 3" };
+
+        public string Duration2 { get; set; } = "Item 2";
+        public IList<string> Durations2 => new List<string> { "Item 1", "Item 2", "Item 3" };
+
+        public AboutViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => OnPropertyChanged(null));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ Test_ComboBox/Test_ComboBox/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Test_ComboBox.ViewModels;
+using Xamarin.Forms;
+
+namespace Test_ComboBox.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
